3bee3a2f026b50ab78dfc38bcc122187bf433dc2
LeChristopher Blackwell <codeblackwell@gmail.comgit status\curl -sSL https://get.rvm.io | bash -s stablegit config --global user.email codeblackwell@gmail.com>
2016-01-21 21:12:28 -0800

    attempting to attach $.count to $.score

--- binarySearchArray/binarySearch.js
+++ binarySearchArray/binarySearch.js
@@ -7,7 +7,18 @@
  * var index = binarySearch([1, 2, 3, 4, 5], 4);
  * console.log(index); // [3]
  */
+//define an iterator
+var i = 0;
 
 var binarySearch = function (array, target) {
- // your code here
+  // your code her
+ 
+  //define base case
+  if(array[i] === target){
+  	return i;
+  }
+  //increment the iterator
+  i++
+  //return recursive statement
+  return binarySearch(array, target)
 };
--- js-quiz-game/assets/app.js
+++ js-quiz-game/assets/app.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-	
+
 	event.preventDefault();
 	// our current count, attached to the upper right hand corner of the browser
-	var $currentCount = $('#counter')
+	var $currentCount = $('#counter');
 	// our current form which will hold the question and subsequent answers loaded in the app.questions object.
 	var $currentForm = $('form');
 	// creates our first random question upon generating the game.html file
 	var firstRandomQuestion = app.questions[Math.floor(Math.random()* app.questions.length)];
-	
+	//isolate the highscore
+	var $highscore = $('.score');
 	//attaches the counter to the highscore on page
-
+	$highscore.prepend($currentCount);
 	//attaches the first question to the form onload
 	$currentForm.prepend('<h3 class = "question"> Question: ' + firstRandomQuestion.question + '</h3>' + '<br><br>');
 
 	// appends all four answers to the corresponding question
 	var answerGenerator = function (randomQuestion){
-
+		$
 	};
 
 	//adds a random question and its corresponding answers to our currentForm
@@ -36,7 +37,7 @@ $(document).ready(function(){
 			$('.choices').empty().append(QandAgenerator());
 		} else {
 			app.failureDisplay();
-		}		
+		}
 	});
 
 	$('#restart').on('click', function(){
@@ -45,4 +46,3 @@ $(document).ready(function(){
 
 	answerGenerator(firstRandomQuestion);
 });
-
